c1ba734a36fefb86fc0a7af991b12ae72fcdcab3
Yash Vinod Sawant <146620043+yashVSawant@users.noreply.github.com>
2023-11-08 20:34:17 +0530

    created arrow function and student object

---
+++ arrowfunction.js
@@ -0,0 +1,11 @@
+let multiply = (a,b)=>a*b;
+
+let student={
+    name:"yash sawant",
+    rollNo:13,
+    std:'final-year',
+    greet(){
+        console.log('hi i am '+this.name);
+    }
+}
+student.greet();
